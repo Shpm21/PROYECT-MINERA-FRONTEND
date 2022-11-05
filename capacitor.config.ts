3a2bd36166bda80ab15586afbeee658e317013e6
Shpm21 <galleguilloscristian2@gmail.com>
2022-11-05 14:15:48 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PROYECT-MINERA-FRONTEND',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
