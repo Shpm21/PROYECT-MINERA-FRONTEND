b67da72e214421374dc6f612794cb9829c22ebc1
Shpm21 <galleguilloscristian2@gmail.com>
2022-11-11 10:43:04 -0400

    delete unused import

--- src/pages/ViewTasksFromEquipment/ViewTasksFromEquipment.tsx
+++ src/pages/ViewTasksFromEquipment/ViewTasksFromEquipment.tsx
@@ -1,27 +1,50 @@
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
   IonPage,
   IonToolbar,
   useIonViewWillEnter
 } from '@ionic/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { Equipment, getEquipment } from '../../data/equipment'
+import { changeDoneStatus, getTasks, Task } from '../../data/task'
+import './ViewTasksFromEquipment.css'
 
 const ViewTasksFromEquipment: React.FC = () => {
   const params = useParams<{ id: string }>()
   const [equipment, setEquipment] = useState<Equipment>()
+  const [tasks, setTasks] = useState<Task[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [changeColor, setChangeColor] = useState<boolean>(false)
 
   useIonViewWillEnter(() => {
     setIsLoading(true)
     const equipment = getEquipment(parseInt(params.id, 10))
     setEquipment(equipment)
+    const t = getTasks()
+    setTasks(t)
     setIsLoading(false)
+    //checkbox-outline
   })
+
+  useEffect(() => {
+    const t = getTasks()
+    setTasks(t)
+  }, [changeColor])
+
+  const changeStatusTask = (task: Task) => {
+    changeDoneStatus(task.id)
+    setTasks(getTasks())
+    setChangeColor(!changeColor)
+  }
+
   return (
     <IonPage id="view-equipment-maintainer-page">
       <IonHeader translucent>
@@ -39,7 +62,33 @@ const ViewTasksFromEquipment: React.FC = () => {
       <IonContent fullscreen>
         {!isLoading ? (
           equipment ? (
-            <></>
+            tasks ? (
+              <>
+                {tasks.map((task) => (
+                  // <IonItem routerLink={`/task/${task.id}`}>
+                  <IonItem lines="none">
+                    <IonButton
+                      slot="start"
+                      class="view-tasks-button"
+                      color={task.done ? 'success' : 'danger'}
+                      onClick={() => {
+                        changeStatusTask(task)
+                      }}
+                    >
+                      <IonIcon
+                        icon="checkbox-outline"
+                        class="view-tasks-icon"
+                      />
+                    </IonButton>
+                    <IonLabel className="ion-text-wrap">
+                      <h3>{task.description}</h3>
+                    </IonLabel>
+                  </IonItem>
+                ))}
+              </>
+            ) : (
+              <div>No hay tareas asignadas para este equipamiento</div>
+            )
           ) : (
             <div>Item no encontrado</div>
           )
@@ -47,6 +96,7 @@ const ViewTasksFromEquipment: React.FC = () => {
           <div>Cargando...</div>
         )}
       </IonContent>
+      <IonButton expand="full">Subir evidencia</IonButton>
     </IonPage>
   )
 }
