496c25738628ace788df39ec3c00c9b4346aef3f
Shpm21 <galleguilloscristian2@gmail.com>
2022-11-13 00:04:22 -0400

    create a FindCategory page

---
+++ src/pages/Admin/Category/FindCategory/FindCategory.tsx
@@ -0,0 +1,57 @@
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonToolbar
+} from '@ionic/react'
+import { useState } from 'react'
+import { Category } from '../../../../data/category'
+import ShortFormCategory from '../components/ShortFormCategory'
+import ShowCategoryInformation from './components/ShowCategoryInformation'
+
+const FindCategory: React.FC = () => {
+  const [category, setCategory] = useState<Category>()
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete()
+    }, 3000)
+  }
+
+  return (
+    <IonPage id="admin-view-category-page">
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/admin" icon="chevron-back-outline" />
+          </IonButtons>
+          <h6>Ver informacion tipo</h6>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+        <ShortFormCategory
+          setCategory={setCategory}
+          textButton="Buscar"
+          placeholder="ID"
+        />
+        {category ? (
+          <ShowCategoryInformation category={category} />
+        ) : (
+          <>
+            <IonItem>Ingresa un Rut válido por favor</IonItem>
+          </>
+        )}
+      </IonContent>
+    </IonPage>
+  )
+}
+
+export default FindCategory
