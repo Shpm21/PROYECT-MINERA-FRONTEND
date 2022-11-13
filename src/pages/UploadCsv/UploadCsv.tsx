fc3aaa38c3d9ac1d0a31fb5f639baf647e2c830e
Shpm21 <galleguilloscristian2@gmail.com>
2022-11-13 19:30:40 -0400

    update a UploadCsv page

--- src/pages/UploadCsv/UploadCsv.tsx
+++ src/pages/UploadCsv/UploadCsv.tsx
@@ -1,25 +1,39 @@
-const UploadCsv: React.FC = () => {
-  return (
-    <>
-      <form className="form-inline">
-        <div className="form-group">
-          <label htmlFor="files">Upload a CSV formatted file:</label>
-          <input
-            type="file"
-            id="files"
-            className="form-control"
-            accept=".csv"
-            required
-          />
-        </div>
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonToolbar
+} from '@ionic/react'
+import UploadCsvForm from './components/UploadCsvForm'
 
-        <div className="form-group">
-          <button type="submit" id="submit-file" className="btn btn-primary">
-            Upload File
-          </button>
-        </div>
-      </form>
-    </>
+const UploadCsv: React.FC = () => {
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete()
+    }, 3000)
+  }
+
+  return (
+    <IonPage id="admin-csv-upload-page">
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/login" icon="chevron-back-outline" />
+          </IonButtons>
+          <h6>Subir csv</h6>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+        <UploadCsvForm />
+      </IonContent>
+    </IonPage>
   )
 }
 
