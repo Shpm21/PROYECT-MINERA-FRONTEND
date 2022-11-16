c7a6f5582acb89f7fbce92817d7d901773f1a0d1
Shpm21 <galleguilloscristian2@gmail.com>
2022-11-16 10:44:53 -0400

    create a ParseServices services

---
+++ src/services/ParseServices.ts
@@ -0,0 +1,19 @@
+export class ParseServices {
+
+}
+// import Papa from 'papaparse';
+
+// export class ParseServices {
+//   private static _instance: ParseServices
+
+//   private constructor() { }
+
+//   public static getInstance(): ParseServices {
+//     if (!ParseServices._instance) ParseServices._instance = new ParseServices()
+//     return ParseServices._instance
+//   }
+
+//   public parseCSVFile(file: File): Promise<any> {
+//     return Papa.parse(file, { file })
+//   }
+// }
